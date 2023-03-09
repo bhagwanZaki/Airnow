dca64ff54b7b91275f7374ebc7ced05c704d14d3
bhagwanZaki <zaki.tarveen@gmail.com>
2023-03-10 02:16:51 +0530

    Minor changes

--- src/component/Navabar.js
+++ src/component/Navabar.js
@@ -1,21 +1,57 @@
 import React from "react";
+
+// custom css
 import "../css/navbar.css";
 
+// imgs
+import bg from "../assets/img/helpCenter.png"
+
+// icons
+import logo from "../assets/logo/logo.png";
+import dashboard from "../assets/logo/dashboard.png";
+import money from "../assets/logo/money.png";
+import web from "../assets/logo/www.png";
+import affiliate from "../assets/logo/affiliate.png";
+import tickets from "../assets/logo/tickets.png";
+import faq from "../assets/logo/faq.png";
+
 function Navabar() {
+  const NavItem = ({ icon, label }) => {
+    return (
+      <div className="navLink">
+        <img src={icon} className="navItemLogo" alt="nav-item-logo" /> {label}
+      </div>
+    );
+  };
   return (
     <div className="navSec">
-      <div className="navHeader">
-        <h3>Airnow</h3>
+      <div className="navDiv">
+        <div className="navHeader">
+          <img src={logo} className="logo" alt="logo" />
+          <h3>Airnow</h3>
+        </div>
+        <div className="navLinks">
+          <div className="navSecHead">MANAGE</div>
+          <NavItem icon={dashboard} label="Proxy" />
+          <NavItem icon={money} label="Balance" />
+          <NavItem icon={web} label="Online check" />
+          <NavItem icon={affiliate} label="Affiliate system" />
+          <div className="navSecHead">SUPPORT</div>
+          <NavItem icon={tickets} label="Tickets" />
+          <NavItem icon={faq} label="FAQ" />
+        </div>
       </div>
-      <div className="navLinks">
-        <div className="navSecHead">MANAGE</div>
-        <div className="navLink">Proxy</div>
-        <div className="navLink">Balance</div>
-        <div className="navLink">Online check</div>
-        <div className="navLink">Affiliate system</div>
-        <div className="navSecHead">SUPPORT</div>
-        <div className="navLink">Tickets</div>
-        <div className="navLink">FAQ</div>
+      <div className="helpCenterContainer">
+        <h2>
+          Help Center
+        </h2>
+        <h3>
+          Visit out help center
+        </h3>
+        <button className="btn">
+          Start
+        </button>
+        <img src={bg} className="bg" alt="help-center" />
       </div>
     </div>
   );
--- src/component/Table.js
+++ src/component/Table.js
@@ -7,48 +7,52 @@ import webmoney from "../assets/logo/webmoney.png";
 function Table() {
   return (
     <table>
-      <tr>
-        <th>Date and time</th>
-        <th>Payment Method</th>
-        <th>Status</th>
-        <th>Amount ($)</th>
-      </tr>
-      <tr>
-        <td>17 Feb 2020, 16:30</td>
-        <td className="cc">
-          <img className="ccic m" src={mastercard} alt="mastercard" />
-          Mastercard
-        </td>
-        <td>Pending</td>
-        <td>- 400.00 $</td>
-      </tr>
-      <tr>
-        <td>14 Feb 2020, 12:39</td>
-        <td className="cc">
-          <img className="ccic p" src={paypal} alt="paypal" />
-          Paypal
-        </td>
-        <td>Pending</td>
-        <td>- 250.00 $</td>
-      </tr>
-      <tr>
-        <td>12 Feb 2020, 09:45</td>
-        <td className="cc">
-          <img className="ccic q" src={qiwi} alt="qiwi" />
-          Qiwi
-        </td>
-        <td>Done</td>
-        <td className="inc">+ 1000.00 $</td>
-      </tr>
-      <tr>
-        <td>10 Feb 2020, 12:39</td>
-        <td className="cc">
+      <thead>
+        <tr>
+          <th>Date and time</th>
+          <th>Payment Method</th>
+          <th>Status</th>
+          <th className="last">Amount ($)</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>17 Feb 2020, 16:30</td>
+          <td className="cc">
+            <img className="ccic m" src={mastercard} alt="mastercard" />
+            Mastercard
+          </td>
+          <td>Pending</td>
+          <td className="last">- 400.00 $</td>
+        </tr>
+        <tr>
+          <td>14 Feb 2020, 12:39</td>
+          <td className="cc">
+            <img className="ccic p" src={paypal} alt="paypal" />
+            Paypal
+          </td>
+          <td>Pending</td>
+          <td className="last">- 250.00 $</td>
+        </tr>
+        <tr>
+          <td>12 Feb 2020, 09:45</td>
+          <td className="cc">
+            <img className="ccic q" src={qiwi} alt="qiwi" />
+            Qiwi
+          </td>
+          <td>Done</td>
+          <td className="inc last">+ 1000.00 $</td>
+        </tr>
+        <tr>
+          <td>10 Feb 2020, 12:39</td>
+          <td className="cc">
             <img className="ccic w" src={webmoney} alt="webmoney" />
-          Webmoney
-        </td>
-        <td>Pending</td>
-        <td>- 250.00 $</td>
-      </tr>
+            Webmoney
+          </td>
+          <td>Pending</td>
+          <td className="last">- 250.00 $</td>
+        </tr>
+      </tbody>
     </table>
   );
 }
